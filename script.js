46c97d0c3ab36e1cbf33032e0f8c796651767ef6
nikkigalusha <nikkigalusha@gmail.com>
2016-05-06 13:20:46 -0700

    Added google book images and titles from API

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function each(coll, f) {
         }
     }
 }
+
 //Creates video playlist
 function createPlaylist(response) {
     // Converts string response into object
@@ -25,9 +26,9 @@ function createPlaylist(response) {
     // Appends video list to the page 
     document.getElementById("video-list").innerHTML = videoList;
 }
+
 //Creates exercise list
 function createExerciseList(response) {
-    console.log(response);
     var exercises = JSON.parse(response);
     var exercisesList = "";
     // Loops through every exercise returned by Khan Academy API and creates list item
@@ -37,20 +38,20 @@ function createExerciseList(response) {
     // Appends exercise list to the page
     document.getElementById("exercises-list").innerHTML = exercisesList;
 }
-//Creates suggested reading list
-function appendBooks (response) {
-    console.log(response);
-    var books = JSON.parse(response);
-    var booksList = "";
-    //Loops through every book return by Google Books API and creates a listed book
-    each(books, function(book) {
-        booksList += '<div><img src='+book.smallThumbnail+'></div>';
-    })
-    //Appends books to the page
-    document.getElementById("book-list").innerHTML = booksList;
 
+//Goal: have list of book images that when clicked takes students to a preview of that book
+//Creates suggested reading list
+function appendBooks(response) {
+    var books = JSON.parse(response);
+    console.log(books);
+    var booksList = "";
+    for (var i = 0; i < books.items.length; i++) {
+        booksList += '<li><a target ="_blank" href ="' + books.items[i].accessInfo.webReaderLink +'"><img src ="' + books.items[i].volumeInfo.imageLinks.smallThumbnail + '"> Title: ' + books.items[i].volumeInfo.title+'</a></li>';
+    }
+     document.getElementById("books-list").innerHTML = booksList;
 }
 
+//Access to Khan Academy API
 function request() {
     // Gets value of user selected topic
     var topic = document.getElementById('topic-selection').value;
@@ -81,17 +82,18 @@ function request() {
 
 }
 
+//Requests access to Google Books API
 function searchGoogle () {
     var xhr = new XMLHttpRequest();
     // Construct API request based off user selected to
-    var url = 'https://www.googleapis.com/books/v1/volumes?q=7th+grade+math&download=epub&key=' + apiKey;
+    var url = 'https://www.googleapis.com/books/v1/volumes?q=7th+grade+math&download=epub&key='+apiKey;
     // Passing callback method to create and append video list if the readyState of the xhr request
     // returns with a 4 ('request finished and response is ready') and status is 200('OK')
     xhr.open("GET", url, true);
     xhr.send();
     // Repeat same action but with exercises
     var bookRequest = new XMLHttpRequest();
-    var bookURL = 'https://www.googleapis.com/books/v1/volumes?q=7th+grade+math&download=epub&key=' + apiKey;
+    var bookURL = 'https://www.googleapis.com/books/v1/volumes?q=7th+grade+math&download=epub&key='+apiKey;
 
     bookRequest.onreadystatechange = function() {
         if (bookRequest.readyState === 4 && bookRequest.status === 200) {
